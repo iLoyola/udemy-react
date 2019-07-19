5904620ec6f628314dc3dfc1fb085757e22fe0a6
Ivan Loyola <ivan@iloyola.com>
2019-07-19 16:06:20 -0400

    add start edit expense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -5,10 +5,11 @@ import {
 	startAddExpense,
 	addExpense,
 	editExpense,
+	startEditExpense,
 	removeExpense,
+	startRemoveExpense,
 	setExpenses,
-	startSetExpenses,
-	startRemoveExpense
+	startSetExpenses
 } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
@@ -55,6 +56,23 @@ test( 'Should edit expense', () => {
 		updates: {
 			note: 'New note value'
 		}
+	});
+});
+
+test( 'Should edit expense from firebase', ( done ) => {
+	const store = createMockStore({});
+	const id = expenses[0].id;
+	const updates = { amount: 21045 };
+	store.dispatch( startEditExpense( id, updates ) ).then( () => {
+		const actions = store.getActions();
+		expect( actions[0] ).toEqual({
+			type: 'EDIT_EXPENSE',
+			id, updates
+		});
+		return database.ref( `expenses/${id}` ).once( 'value' );
+	}).then( ( snapshot ) => {
+		expect( snapshot.val().amount ).toBe( updates.amount );
+		done();
 	});
 });
 
--- src/tests/pages/EditExpensePage.test.js
+++ src/tests/pages/EditExpensePage.test.js
@@ -4,15 +4,15 @@ import { shallow } from 'enzyme';
 import { EditExpensePage } from '../../pages/EditExpensePage';
 import expenses from '../fixtures/expenses';
 
-let editExpense, startRemoveExpense, history, wrapper;
+let startEditExpense, startRemoveExpense, history, wrapper;
 
 beforeEach( () => {
-	editExpense = jest.fn();
+	startEditExpense = jest.fn();
 	startRemoveExpense = jest.fn();
 	history = {push: jest.fn()};
 	wrapper = shallow(
 		<EditExpensePage
-			editExpense={editExpense}
+			startEditExpense={startEditExpense}
 			startRemoveExpense={startRemoveExpense}
 			history={history}
 			expense={expenses[2]}
@@ -27,7 +27,7 @@ test( 'Should render Edit Expense Page', () => {
 test( 'Should handle edit on submit', () => {
 	wrapper.find( 'ExpenseForm' ).prop( 'onSubmit' )( expenses[2] );
 	expect( history.push ).toHaveBeenLastCalledWith( '/' );
-	expect( editExpense ).toHaveBeenLastCalledWith( expenses[2].id, expenses[2] );
+	expect( startEditExpense ).toHaveBeenLastCalledWith( expenses[2].id, expenses[2] );
 });
 
 test( 'Should handle remove on submit', () => {
